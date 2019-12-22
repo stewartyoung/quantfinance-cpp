735e4b871ed9f75c84b3dd1e9e4fbdf909633e5e
Stewart Young <stewartyoung97@gmail.com>
2019-12-22 09:44:51 +0000

    working on put call parity pricing

--- PutCallParity.cpp
+++ PutCallParity.cpp
@@ -1,15 +1,22 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-int putCallPrice (int optionPrice, string optionType)
+int putCallPrice (double underlyingPrice, double maturityTime, double strikePrice , double riskFreeRate, double optionPrice, string optionType )
 {
-    double parityPrice ;
-    if (optionType.compare("p") != 0)
+    double parityPrice, discountedStrike;
+    string parityType;
+    discountedStrike = exp(- riskFreeRate * maturityTime);
+    // if the option type is a put
+    if (optionType.compare("p") == 0)
     {
-        parityPrice = optionPrice * 2;
+        parityType = "call";
+        parityPrice = optionPrice + underlyingPrice - strikePrice * discountedStrike;
     }
-    if (optionType.compare("f") != 0)
+    // if the option type is a call
+    if (optionType.compare("c") == 0)
     {
+        parityType = "put";
         parityPrice = optionPrice * 2;
     }
 
@@ -28,8 +35,8 @@ int main()
     printf("Option Price %f\n", optionPrice);
     
     double parityPrice;
-    parityPrice = putCallPrice(optionPrice, optionType);
-    cout << parityPrice;
+    parityPrice = putCallPrice(10.4, 5.0, 10.0, 2.0, optionPrice, optionType);
+    printf ("The parity price of the opposite option is %f", parityPrice);
     return 0;
 }
 
